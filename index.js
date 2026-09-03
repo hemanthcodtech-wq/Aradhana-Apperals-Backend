400f945093fdcd05290e689b69ab4f457e9702ff
chinna0107 <kancharlahemanth89@gmail.com>
2026-09-03 14:58:43 +0530

    Production 2.23

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors({
     const allowed = [
       'http://localhost:5173',
       'http://localhost:3000',
-      'https://nightpants.vercel.app',
+      'https://aradhana-apperals-frontend.vercel.app',
       process.env.FRONTEND_URL,
     ].filter(Boolean);
 
